e465ddb9eda08ca343e3f20d7c631ae51dafa6f4
Christopher Punt <christopherpunt@outlook.com>
2022-05-27 23:34:03 -0400

    setting up project

---
+++ src/unitTest/MyClassTest.java
@@ -0,0 +1,13 @@
+package unitTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class MyClassTest{
+    
+    @Test
+    public void firstTest(){
+        assertEquals(2, 1 + 1);
+    }
+}
